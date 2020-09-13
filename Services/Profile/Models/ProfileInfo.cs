341e920e5b07b59a8de3c32b38780689990c9f33
Khiem <ngockhiem27@gmail.com>
2020-09-13 10:21:23 +0700

    add json property format

--- Services/Profile/Models/ProfileInfo.cs
+++ Services/Profile/Models/ProfileInfo.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Profile.Models
 {
     public class ProfileInfo
     {
+        [JsonPropertyName("user")]
         public UserInfo User { get; set; }
+        [JsonPropertyName("games")]
         public List<GameInfo> Games { get; set; }
     }
 }
